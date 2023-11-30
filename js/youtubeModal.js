8d7574a88141b594ebaaafbae7d069b33e2507d0
evanemoy <nemoykina00@inbox.ru>
2023-11-30 13:40:45 +0300

    fixed scrollfilm

--- js/youtubeModal.js
+++ js/youtubeModal.js
@@ -1,11 +1,14 @@
 const watchTrailer = document.querySelector('.watchTrailer');
+const html = document.querySelector('html');
 const youtubeContainer = document.querySelector('.youtubeContainer');
 const iframe = document.querySelector('.youtubeContainer > iframe')
 watchTrailer.addEventListener('click',()=>{
     youtubeContainer.classList.remove("closed");
+    html.classList.add('scrollDisabled');
 })
 youtubeContainer.addEventListener('click',(e)=>{
     if (e.target != iframe){
         youtubeContainer.classList.add('closed');
+        html.classList.remove('scrollDisabled');
     }
 })
